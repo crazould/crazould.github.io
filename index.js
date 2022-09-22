643b5824864579932d8bfe88aa2fb7f7962032a6
crazould <53102589+crazould@users.noreply.github.com>
2022-09-22 07:54:26 +0700

    configure tailwind npm

--- index.js
+++
@@ -1,9 +0,0 @@
-const img = document.querySelector('.profile-pic')
-
-img.addEventListener('mouseover', () => {
-  img.src = './assets/profile-pic-cat.png'
-})
-
-img.addEventListener('mouseout', () => {
-  img.src = './assets/profile-pic.png'
-})
