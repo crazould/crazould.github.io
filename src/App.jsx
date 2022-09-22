55206617bc9f24dd8126cd7167307c9beb57d309
crazould <53102589+crazould@users.noreply.github.com>
2022-09-23 06:55:43 +0700

    refactor css

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import './index.css'
 function App() {
 
   return (
-    <div className="min-h-screen bg-zinc-900 body">
-      <div className="wrapper p-5 flex justify-center flex-wrap">
-        <div className="profile-intro p-2 mb-5">
-          <div className="profile-intro-desc flex flex-col items-center mb-3 md:mb-5 relative w-[280px] md:w-[400px]">
-            <div className="text-5xl -skew-y-6 md:text-[2rem] text-center absolute text-blue-600">
+    <div className="body">
+      <div className="wrapper ">
+        <div className="profile-intro">
+          <div className="profile-intro-desc">
+            <div className="profile-job-out">
               FRONTEND DEVELOPER
               FRONTEND DEVELOPER
               FRONTEND DEVELOPER
@@ -17,8 +17,8 @@ function App() {
               FRONTEND DEVELOPER
               FRONTEND DEVELOPER
             </div>
-            <div className="profile-pic-container bg-blue-600 w-[280px] md:w-[400px] rounded-full relative overflow-hidden">
-              <div className="text-bg text-5xl -skew-y-6 md:text-[2rem] w-[280px] md:w-[400px] text-center absolute z-10">
+            <div className="profile-pic-container">
+              <div className="profile-job-in">
                 FRONTEND DEVELOPER
                 FRONTEND DEVELOPER
                 FRONTEND DEVELOPER
@@ -27,26 +27,26 @@ function App() {
                 FRONTEND DEVELOPER
                 FRONTEND DEVELOPER
               </div>
-              <img src={profilePic} alt="profile picture" className="profile-pic z-50 relative w-full" />
+              <img src={profilePic} alt="profile picture" className="profile-pic" />
             </div>
-            <div className="profile-name text-2xl -skew-y-6 md:text-4xl border-b-2 hover:border-blue-600 text-center z-20 relative duration-200">
+            <div className="profile-name">
                 Muhammad At Thariq Filardi
             </div>
           </div>
-          <div className="profile-social -skew-y-6 text-blue-600 flex justify-evenly text-4xl md:text-5xl">
+          <div className="profile-social">
             <a target="_blank" href="https://www.linkedin.com/in/muhammad-filardi/">
-              <i className="uil uil-linkedin hover:text-zinc-100 duration-200"></i>
+              <i className="uil uil-linkedin"></i>
             </a>
             <a target="_blank" href="https://github.com/crazould">
-              <i className="uil uil-github hover:text-zinc-100 duration-200"></i>
+              <i className="uil uil-github"></i>
             </a>
             <a href="mailto:crazould@gmail.com">
-              <i className="uil uil-envelope hover:text-zinc-100 duration-200"></i>
+              <i className="uil uil-envelope"></i>
             </a>
           </div>
         </div>
-        <div className="profile-desc flex flex-col items-center">
-          <div className="text-zinc-100">
+        <div className="profile-desc">
+          <div className="">
             <ul>
               <li>
                 EDUCATION
