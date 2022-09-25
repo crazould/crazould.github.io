229f3bff6ac91c8597b077ea2e9399868c82c60f
crazould <53102589+crazould@users.noreply.github.com>
2022-09-25 22:56:05 +0700

    adjust desc item

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,89 @@
-import profilePic from './assets/profile-pic.png'
-import profilePicShine from './assets/profile-pic-shine.png'
-import './index.css'
-import { useState } from 'react'
+import profilePic from "./assets/profile-pic.png";
+import profilePicShine from "./assets/profile-pic-shine.png";
+import "./index.css";
+import { useState } from "react";
 
-const profileJobTxt = 'FRONTEND DEVELOPER '
-const totalProfileJobTxt = 10
-let arr = []
-for (let index = 0; index < totalProfileJobTxt; index++) {
-  arr = [...arr, profileJobTxt]
-}
+const DescItem = ({ title, currIdx, viewIdx, setViewIdx }) => {
+  return (
+    <li
+      className={
+        viewIdx === currIdx
+          ? `active desc-item item-${currIdx}`
+          : viewIdx !== -1
+          ? "disable desc-item"
+          : "desc-item"
+      }
+      onClick={() => {
+        console.log({ currIdx, viewIdx });
+        setViewIdx(currIdx);
+      }}
+    >
+      {title}
+      <ul className="content">
+        <li>asdfasdf</li>
+        <li>asdfasdf</li>
+        <li>asdfasdf</li>
+      </ul>
+    </li>
+  );
+};
 
 function App() {
-
   const [isHoverPic, setHoverPic] = useState(false);
+  const profileJobTxt = "FRONTEND DEVELOPER ";
+
+  const totalProfileJobTxt = 10;
+  let ProfileJob = [];
+  for (let index = 0; index < totalProfileJobTxt; index++) {
+    ProfileJob = [...ProfileJob, profileJobTxt];
+  }
+
+  const [viewIdx, setViewIdx] = useState(-1);
+
+  const profileDescTxt = [
+    "EDUCATION",
+    "WORK EXPERIENCE",
+    "SKILLS",
+    "ACHIEVEMENT",
+    "CERTIFICATE",
+    "PROJECTS",
+  ];
+
+  const DescItems = profileDescTxt.map((title, index) => {
+    return (
+      <DescItem
+        title={title}
+        key={index}
+        currIdx={index}
+        viewIdx={viewIdx}
+        setViewIdx={setViewIdx}
+      />
+    );
+  });
 
   return (
     <div className="body">
       <div className="wrapper ">
         <div className="profile-intro">
           <div className="profile-intro-desc">
-            <div className="profile-job-out">
-                {arr}
-            </div>
+            <div className="profile-job-out">{ProfileJob}</div>
             <div className="profile-pic-container">
-              <div className="profile-job-in">
-                {arr}
-              </div>
-              <img src={isHoverPic ? profilePicShine:profilePic} alt="profile picture" className="profile-pic" onMouseEnter={()=> setHoverPic(true)} onMouseOut={()=> setHoverPic(false)} />
+              <div className="profile-job-in">{ProfileJob}</div>
+              <img
+                src={isHoverPic ? profilePicShine : profilePic}
+                alt="profile picture"
+                className="profile-pic"
+                onMouseEnter={() => setHoverPic(true)}
+                onMouseOut={() => setHoverPic(false)}
+              />
             </div>
-            <div className="profile-name">
-                Muhammad At Thariq Filardi
-            </div>
+            <div className="profile-name">Muhammad At Thariq Filardi</div>
           </div>
           <div className="profile-social">
-            <a target="_blank" href="https://www.linkedin.com/in/muhammad-filardi/">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/muhammad-filardi/"
+            >
               <i className="uil uil-linkedin"></i>
             </a>
             <a target="_blank" href="https://github.com/crazould">
@@ -44,17 +94,20 @@ function App() {
             </a>
           </div>
         </div>
-        <ul className="profile-desc">
-          <li className='desc-item'>EDUCATION</li>
-          <li className='desc-item'>JOB EXPERIENCE</li>
-          <li className='desc-item'>SKILLS</li>
-          <li className='desc-item'>ACHIEVEMENT</li>
-          <li className='desc-item'>CERTIFICATE</li>
-          <li className='desc-item'>PROJECTS</li>
-        </ul>
+        <div className="profile-desc-wrapper">
+          <div
+            className="profile-desc-header"
+            onClick={() => {
+              setViewIdx(-1);
+            }}
+          >
+            {viewIdx === -1 ? "About Me" : "Back"}
+          </div>
+          <ul className="profile-desc">{DescItems}</ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
